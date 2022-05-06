490f30de3c24a75facef1b45ff59666d457e6031
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-05-06 15:30:37 +0200

    added styles for container and added border

--- src/containers/todoContainer.vue
+++ src/containers/todoContainer.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="hello">
-    <input type="text" placeholder="wprowadź zadanie" v-model="newTask.title" />
+    <div class="container">
+        <div class="container--border">
+            <input type="text" placeholder="wprowadź zadanie" v-model="newTask.title" />
 
-    <div>
-        <select 
-            name='categories' 
-            id='categories' 
-            v-model='newTask.category' 
-        >
-            <option 
-                v-for='category in categories' :value='category'
-                v-bind:key='category'    
-            >
-                {{ category }}
-            </option>  
-        </select>
+            <div>
+                <select 
+                    name='categories' 
+                    id='categories' 
+                    v-model='newTask.category' 
+                >
+                    <option 
+                        v-for='category in categories' :value='category'
+                        v-bind:key='category'    
+                    >
+                        {{ category }}
+                    </option>  
+                </select>
+            </div>
+
+            <div>
+                <input type='checkbox' v-model='newTask.important' />
+                <label> ważne </label>
+            </div>
+
+            <button @click="addTask">
+                dodaj zadanie
+            </button>
+
+            <ul v-for="task in tasks" v-bind:key="task.id">
+                <li>{{ task.title }}</li>
+            </ul>
+
+        </div>
     </div>
-
-    <div>
-        <input type='checkbox' v-model='newTask.important' />
-        <label> ważne </label>
-    </div>
-
-    <button @click="addTask">
-        dodaj zadanie
-    </button>
-
-    <ul v-for="task in tasks" v-bind:key="task.id">
-        <li>{{ task.title }}</li>
-    </ul>
-
-  </div>
 </template>
 
 <script>
@@ -67,5 +69,17 @@ export default {
 </script>
 
 <style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
+    .container--border{
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        box-shadow: 6px 6px 10px 0px rgb(42, 43, 49);
+    }
 </style>
